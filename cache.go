6bc1bfac0eb8a4e4b46b408bb5716b41dfa49f6a
Tleukulov Amankos <amankos.t@bilimland.com>
2023-01-16 16:53:05 +0600

    add timer

--- cache.go
+++ cache.go
@@ -3,28 +3,40 @@ package in_memory_cache
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
+var timer *time.Timer
+
 type Cache struct {
-	m map[string]interface{}
+	m  map[string]interface{}
+	mu *sync.Mutex
 }
 
 func New() *Cache {
 	return &Cache{
-		m: make(map[string]interface{}),
+		m:  make(map[string]interface{}),
+		mu: new(sync.Mutex),
 	}
 }
 
 func (c Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.mu.Lock()
 	c.m[key] = value
-	delete := func() {
+	timer.Reset(ttl)
+	c.mu.Unlock()
+
+	go func() {
+		<-timer.C
 		c.Delete(key)
-	}
-	go time.AfterFunc(ttl, delete)
+	}()
 }
 
 func (c Cache) Get(key string) (interface{}, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	val, ok := c.m[key]
 	if ok {
 		return val, nil
@@ -34,5 +46,7 @@ func (c Cache) Get(key string) (interface{}, error) {
 }
 
 func (c Cache) Delete(key string) {
+	c.mu.Lock()
 	delete(c.m, key)
+	c.mu.Unlock()
 }
